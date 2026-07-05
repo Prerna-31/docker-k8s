ff0e4f62fd9ac1b23170888efbcdccb8f9a8a7e4
Prerna-31 <64802307+Prerna-31@users.noreply.github.com>
2026-07-05 13:44:51 +0530

    Add files via upload

---
+++ multiContainer_complex_k8s/client/src/Fib.js
@@ -0,0 +1,86 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Fib = () => {
+  const [seenIndexes, setSeenIndexes] = useState([]);
+  const [values, setValues] = useState({});
+  const [index, setIndex] = useState("");
+
+  useEffect(() => {
+    fetchValues();
+    fetchIndexes();
+  }, []);
+
+  const fetchValues = async () => {
+    try {
+      const values = await axios.get("/api/values/current");
+      setValues(values.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const fetchIndexes = async () => {
+    try {
+      const seenIndexes = await axios.get("/api/values/all");
+      setSeenIndexes(seenIndexes.data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await axios.post("/api/values", {
+        index: index,
+      });
+      setIndex("");
+      fetchValues();
+      fetchIndexes();
+    } catch (err) {
+      console.error(err);
+      alert("Submit failed. Check that postgres and the server are running.");
+    }
+  };
+
+  const renderSeenIndexes = () => {
+    return seenIndexes.map(({ number }) => number).join(", ");
+  };
+
+  const renderValues = () => {
+    const entries = [];
+
+    for (let key in values) {
+      entries.push(
+        <div key={key}>
+          For index {key} I calculated {values[key]}
+        </div>
+      );
+    }
+
+    return entries;
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Enter your index:</label>
+        <input
+          value={index}
+          onChange={(event) => setIndex(event.target.value)}
+        />
+        <button>Submit</button>
+      </form>
+
+      <h3>Indexes I have seen:</h3>
+      {renderSeenIndexes()}
+
+      <h3>Calculated Values:</h3>
+      {renderValues()}
+    </div>
+  );
+};
+
+export default Fib;
